289e5ec2df023b10875ce043f63345fca0d8c638
Maciej Zalewski <39594034+Hinhu@users.noreply.github.com>
2018-12-03 23:19:03 +0100

    Add files via upload

---
+++ src/hashsystem/RSA.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashsystem;
+
+/**
+ *
+ * @author Maciek
+ */
+public class RSA {
+    public static String encode(PublicKey key, String text){return null;}
+    public static String decode(PrivateKey key, String crytpogram){return null;}
+    public static Key[] generateKeys(long p,long q){return null;}
+}
