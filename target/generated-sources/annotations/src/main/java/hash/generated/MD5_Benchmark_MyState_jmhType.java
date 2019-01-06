d74fd72bcb29897a9a338e17683a86e8c370ab0c
Janusz Sawicki <janusz_sawicki97@o2.pl>
2019-01-06 03:39:55 +0100

    Cleaned accidental mess

--- target/generated-sources/annotations/src/main/java/hash/generated/MD5_Benchmark_MyState_jmhType.java
+++
@@ -1,4 +0,0 @@
-package src.main.java.hash.generated;
-public class MD5_Benchmark_MyState_jmhType extends MD5_Benchmark_MyState_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/src/main/java/hash/generated/MD5_Benchmark_jmhType.java
+++
@@ -1,4 +0,0 @@
-package src.main.java.hash.generated;
-public class MD5_Benchmark_jmhType extends MD5_Benchmark_jmhType_B3 {
-}
-
